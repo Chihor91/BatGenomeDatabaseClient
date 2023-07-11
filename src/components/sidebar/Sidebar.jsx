deaaa95ce7ad1fd6b4bb238f6bb69da49fb260ca
ngarcia <ngarcia@up.edu.ph>
2023-07-12 01:24:12 +0800

    Revise sidebar design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -30,24 +30,11 @@ export default class Sidebar extends Component {
   render() {
     return (
       <div className="sidebarFormat">
-        <IconButton aria-label="menu" onClick={this.toggle_show} sx={{color: "#101010"}}>
+        <IconButton className="togglebtn" onClick={this.toggle_show} sx={{color: "#101010"}}>
           <MenuIcon/>
         </IconButton>
         {/* &emsp; <Breadcrumb Crumb={this.props.Crumb} Match={this.props.Match} /> */}
-        <div className={this.state.classname}>
-          <svg
-            className="w-6 h-6 closebtn"
-            fill="white"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={this.toggle_show}
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
+        <div className={this.state.classname} onClick={this.toggle_show}>
           <Link to="/">Home</Link>
           <Link to="/dashboard">Dashboard</Link>
           <Link to="/about">About</Link>
