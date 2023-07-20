88094564133f5f13ef0b85008d5f2e49275c6b8d
ngarcia <ngarcia@up.edu.ph>
2023-07-20 15:43:29 +0800

    Revise taxonomy page implementation

--- src/pages/taxonomy/Taxonomy.jsx
+++ src/pages/taxonomy/Taxonomy.jsx
@@ -1,19 +1,20 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router";
-import TaxonomyHandler from "../../components/taxonomy_handler/TaxonomyHandler";
-import Breadcrumb from "../../components/sidebar/Breadcrumb";
+import React from "react";
 
-export class Taxonomy extends Component {
-  render() {
-    return (
-      <div>
-        <Breadcrumb Crumb={this.props.Crumb} Match={this.props.match} />
-        <div className="taxonomy">
-          <TaxonomyHandler />
-        </div>
+export default function Taxonomy() {
+  return(
+    <div>
+      <div className="taxonomy-list">
+        <h2>Select Category</h2>
+        <ul className="taxonomy-list">
+          <li>Domain</li>
+          <li>Phylum</li>
+          <li>Class</li>
+          <li>Order</li>
+          <li>Family</li>
+          <li>Genus</li>
+          <li>Species</li>
+        </ul>
       </div>
-    );
-  }
+    </div>
+    )
 }
-
-export default withRouter(Taxonomy);
