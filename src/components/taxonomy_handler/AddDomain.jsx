49b0bdb2e8f55240389a89f5112b33f86cea1722
ngarcia <ngarcia@up.edu.ph>
2023-07-20 15:50:12 +0800

    Remove taxonomy handler

--- src/components/taxonomy_handler/AddDomain.jsx
+++ src/components/taxonomy_handler/AddDomain.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./TaxonomyHandler";
 import axios from "axios";
 import { categoryContents } from "./CategoryContents";
 import ParentList from "./ParentList";
