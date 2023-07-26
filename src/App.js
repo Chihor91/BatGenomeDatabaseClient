d964c6ac9b091016d06668a4928ff1b3c69b3fc1
ngarcia <ngarcia@up.edu.ph>
2023-07-26 18:08:39 +0800

    Add authentication provider

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { AuthProvider } from "./context/AuthContext"
 import TopBar from "./components/topbar/Topbar";
 import { Route, Router, Switch } from "react-router-dom";
 import routes from "./Routes";
@@ -12,7 +13,7 @@ if (localStorage.logged_in === undefined) {
 axios.defaults.baseURL = "http://127.0.0.1:8000/";
 // axios.defaults.baseURL = "http://202.92.144.124:8000/";
 axios.defaults.headers.common["Authorization"] =
-  "Bearer " + localStorage.access;
+  "Bearer " + JSON.parse(localStorage.getItem('authTokens')).access;
 
 // window.onScroll = (e) => console.log("scrolled!");
 
@@ -20,39 +21,40 @@ function App() {
   return (
     <div className="App">
       <Router history={history}>
-        <TopBar />
-
-        <Switch>
-          {routes.map((route, route_id) => {
-            return (
-              <Route
-                key={route_id}
-                path={route.path}
-                render={() => {
-                  if (route.component === CategoryList) {
-                    return (
-                      <route.component
-                        Category={
-                          (route.name = route.name
-                            .charAt(0)
-                            .toUpperCase()
-                            .concat(route.name.slice(1)))
-                        }
-                        Crumb={route.path}
-                      />
-                    );
-                  } else {
-                    return (
-                      <route.component
-                        Crumb={route.path}
-                      />
-                    );
-                  }
-                }}
-              />
-            );
-          })}
-        </Switch>
+        <AuthProvider>
+          <TopBar />
+          <Switch>
+            {routes.map((route, route_id) => {
+              return (
+                <Route
+                  key={route_id}
+                  path={route.path}
+                  render={() => {
+                    if (route.component === CategoryList) {
+                      return (
+                        <route.component
+                          Category={
+                            (route.name = route.name
+                              .charAt(0)
+                              .toUpperCase()
+                              .concat(route.name.slice(1)))
+                          }
+                          Crumb={route.path}
+                        />
+                      );
+                    } else {
+                      return (
+                        <route.component
+                          Crumb={route.path}
+                        />
+                      );
+                    }
+                  }}
+                />
+              );
+            })}
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
