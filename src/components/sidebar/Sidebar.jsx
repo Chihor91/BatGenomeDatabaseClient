18d8d614c3033d45550c359195ee21469710442e
ngarcia <ngarcia@up.edu.ph>
2023-07-28 15:52:37 +0800

    Revise sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,48 +1,39 @@
-import React, { Component } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 import MenuIcon from '@mui/icons-material/Menu';
 import { IconButton } from "@mui/material";
+import AuthContext from "../../context/AuthContext";
 
-export default class Sidebar extends Component {
-  constructor(props) {
-    super(props);
+export default function Sidebar({ sidebar, setSidebar, taxSidebar, setTaxSidebar }) {
+  let {user, logoutUser} = useContext(AuthContext)
+  const [active, setActive] = useState("sidebar_inactive");
+  useEffect(() => {
+    sidebar ? setActive("sidebar") : setActive("sidebar_inactive")
+  })
 
-    this.state = {
-      classname: "sidebar_inactive",
-    };
-
-    this.toggle_show = this.toggle_show.bind(this);
-  }
-
-  toggle_show() {
-    if (this.state.classname === "sidebar_inactive") {
-      this.setState({
-        classname: "sidebar",
-      });
-    } else {
-      this.setState({
-        classname: "sidebar_inactive",
-      });
-    }
-  }
-
-  render() {
-    return (
-      <div className="sidebarFormat">
-        <IconButton className="togglebtn" onClick={this.toggle_show} sx={{color: "#101010"}}>
+  return(
+    <div className="sidebarFormat">
+        <IconButton className="togglebtn" onClick={() => setSidebar(!sidebar)} sx={{color: "#101010"}}>
           <MenuIcon/>
         </IconButton>
         {/* &emsp; <Breadcrumb Crumb={this.props.Crumb} Match={this.props.Match} /> */}
-        <div className={this.state.classname} onClick={this.toggle_show}>
+        <div className={active} onClick={() => setSidebar(!sidebar)}>
           <Link to="/">Home</Link>
           {/* <Link to="/dashboard">Dashboard</Link> */}
           <Link to="/about">About</Link>
           <Link to="/strain">Strain</Link>
-          <Link to="/taxonomy">Taxonomy</Link>
-          <Link to="/login">Login</Link>
+          <a onClick={() => setTaxSidebar(!taxSidebar)}>Taxonomy</a>
+          {
+            user ?
+            <>
+            <Link to="/profile">Profile</Link>
+            <a onClick={logoutUser}>Logout</a>
+            </>
+            :
+            <Link to="/login">Login</Link>
+          }
         </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
