c6b5e619650cf5de1164fccd93be1cfab62100d4
ngarcia <ngarcia@up.edu.ph>
2023-08-02 13:15:37 +0800

    Add context provider to profile page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import "./Profile.css";
+import AuthContext from "../../context/AuthContext";
 
-export class Profile extends Component {
-  render() {
+export default function Profile(){
+  let { user } = useContext(AuthContext)
     return (
       <div className="profile_container">
         <div className="profile_header">
           <div className="profile_header_left">
             <div className="pic_container">picture</div>
             <div className="name_container">
-              {localStorage.first_name} {localStorage.last_name}
+              {user.first_name} {user.last_name}
             </div>
           </div>
 
@@ -18,27 +19,24 @@ export class Profile extends Component {
 
         <div className="profile_information">
           <div className="profile_item">
-            username <i>{localStorage.username}</i>
+            username <i>{user.username}</i>
           </div>
           <div className="profile_item">
-            email <i>{localStorage.email}</i>
+            email <i>{user.email}</i>
           </div>
           <div className="profile_item">
-            mobile_number <i>{localStorage.mobile_number}</i>
+            mobile_number <i>{user.mobile_number}</i>
           </div>
           <div className="profile_item">
-            landline_number <i>{localStorage.landline_number}</i>
+            landline_number <i>{user.landline_number}</i>
           </div>
           <div className="profile_item">
-            address <i>{localStorage.address}</i>
+            address <i>{user.address}</i>
           </div>
           <div className="profile_item">
-            role <i>{localStorage.role}</i>
+            role <i>{user.role}</i>
           </div>
         </div>
       </div>
     );
-  }
 }
-
-export default Profile;
