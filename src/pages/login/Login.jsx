a789a07c252752016b629b42a676830ba65bce96
ngarcia <ngarcia@up.edu.ph>
2023-08-02 13:15:20 +0800

    Revise login form style

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,23 @@
 import React, { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
-
+import "./Login.css"
 
 export default function Login(){
   let { loginUser } = useContext(AuthContext)
   return (
-    <div>
-        <form onSubmit={loginUser}>
-          <input type="text" name="username" placeholder="Enter username" />
-          <input type="password" name="password" placeholder="Enter password" />
-          <input type="submit" />
+    <body>
+      <div class="wrapper">
+        <form className="loginform" onSubmit={loginUser}>
+            <h1>Login</h1>
+            <div class="input-box">
+              <input type="text" name="username" placeholder="Enter username" />
+            </div>
+            <div class="input-box">
+              <input type="password" name="password" placeholder="Enter password" />
+            </div>
+            <button type="submit" class="login_btn">Login</button>
         </form>
       </div>
-    );
+    </body>
+  );
 }
