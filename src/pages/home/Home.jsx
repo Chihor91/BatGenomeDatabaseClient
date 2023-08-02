13595d4424d8d3db50837f44ea75777f06bcc39c
ngarcia <ngarcia@up.edu.ph>
2023-08-02 13:14:51 +0800

    Revise homepage and about style

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,8 @@ import "./Home.css";
 
 export default function Home(){
     return (
-      <div>
-        <div className="homepage">
-          <h1>Welcome to the Home Page!</h1>
-        </div>
+      <div class="homepage">
+        <h1>Welcome to the Home Page!</h1>
       </div>
     );
 }
